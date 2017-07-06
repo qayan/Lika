c55cb99a24570bed411299d1c5e2c7302ad948b5
qayan <kayananjelika01@gmail.com>
2017-07-06 15:16:57 +0400

    Update cf.js

--- cf.js
+++ cf.js
@@ -1,7 +1,14 @@
 function fToC(f) {
   c = (f - 32) * 5 / 9;
   return c;
-}function cToF(c) {
+}
+
+function cToF(c) {
   f = c * 9/5 + 32 
   return f
+}
+
+function speak(text) {
+  var msg = new SpeechSynthesisUtterance('Hello World');
+  window.speechSynthesis.speak(msg);
 }
